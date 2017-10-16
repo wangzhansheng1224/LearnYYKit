a916266efd4caf05c2a084a01c6121cd2ba49507
wangzs <wangzs@smartdot.com>
2017-10-16 15:21:01 +0800

    初版

---
+++ LearnYYKit/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LearnYYKit
+//
+//  Created by 王战胜 on 2017/10/16.
+//  Copyright © 2017年 gocomtech. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LearnYYKit/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LearnYYKit
+//
+//  Created by 王战胜 on 2017/10/16.
+//  Copyright © 2017年 gocomtech. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
